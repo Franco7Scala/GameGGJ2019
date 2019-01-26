16030e03ff3e06a9db4a1b53952834d1b4993c9f
mym2olab <domenico.vnt@gmail.com>
2019-01-26 17:19:11 +0100

    PlayerMovement PlayerRotation Timer and HealthBar manager

---
+++ _Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private float health = 100f;
+
+    public float Health { get => health; set => health = value; }
+
+    public void EnemyAttack(float quantity)
+    {
+        if(Health <= 0)
+        {
+            return;
+        }
+
+        Health -= quantity;
+    }
+
+
+}
