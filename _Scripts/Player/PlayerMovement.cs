f7effe4be54da967a8488e5511d5e33e593f870e
mym2olab <domenico.vnt@gmail.com>
2019-01-26 11:49:06 +0100

    Removed rigidbody component and used character controller instead

--- _Scripts/Player/PlayerMovement.cs
+++ _Scripts/Player/PlayerMovement.cs
@@ -1,26 +1,98 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+//[RequireComponent(typeof(Health))]
+//[RequireComponent(typeof(Attacker))]
+//[RequireComponent(typeof(PlayerAnimator))]
 public class PlayerMovement : MonoBehaviour
 {
-    public float playerMovementStep = 1f;
+    //public VirtualPad pad;
+    public float walkSpeed = 6.0f;
+    public float runSpeed = 13.0f;
+    public float gravity = -9.8f;
+    public int maxJumpSteps = 6;
 
-    private Rigidbody playerRb;
+    private CharacterController controller;
+    //private Health health;
+    //private PlayerAnimator playerAnimator;
+    //private Attacker attacker;
+    private int jumpSteps = Int32.MaxValue;
+    private bool running = false;
+    private float speed = 0;
 
-    private void Start()
+
+    void Start()
     {
-        playerRb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+        //health = GetComponent<Health>();
+        //attacker = GetComponent<Attacker>();
+        //playerAnimator = GetComponent<PlayerAnimator>();
+        speed = walkSpeed;
     }
 
-    private void FixedUpdate()
+    void Update()
     {
-        float h = Input.GetAxis("Horizontal") * playerMovementStep;
-        float v = Input.GetAxis("Vertical") * playerMovementStep;
+        // Movement
+        float h = -Input.GetAxis("Horizontal");
+        float v = -Input.GetAxis("Vertical");
+        //Vector3 movement = new Vector3(0, 0, (pad.Vertical() * speed));
+        Vector3 movement = new Vector3(h * speed, 0, v * speed);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        // Jump
+        if (jumpSteps < maxJumpSteps)
+        {
+            jumpSteps++;
+            movement.y = -gravity;
+        }
+        else
+        {
+            movement.y = gravity;
+        }
+        // Run
+        Run();
 
-        Vector3 vel = playerRb.velocity;
-        vel.x = h;
-        vel.z = v;
-        playerRb.velocity = vel;
+        // Finalizing
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        controller.Move(movement);
+    }
+
+    public void Jump()
+    {
+        jumpSteps = 0;
+    }
+
+    public void StartRun()
+    {
+        running = true;
+        //health.DecreaseFitness();
+    }
+
+    public void StopRun()
+    {
+        running = false;
+        //health.IncreaseFitness();
+    }
+
+    public void Attack()
+    {
+        //playerAnimator.AnimateArm();
+        //attacker.Hurt();
+    }
+
+    private void Run()
+    {
+        if (running /*&& health.fitnessAvailable()*/)
+        {
+            speed = runSpeed;
+        }
+        else
+        {
+            speed = walkSpeed;
+            //health.IncreaseFitness();
+        }
     }
 }
